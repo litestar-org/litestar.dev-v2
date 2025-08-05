1074a4655503601b32705ecf40d88ba77c0baadf
Julien <courtes.julien@gmail.com>
2025-08-05 09:31:51 +0200

    fix: error with better-sqlite3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,30 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+  content: {
+    experimental: { nativeSqlite: true },
+  },
+  ui: {
+    colorMode: {
+      preference: 'light'
+    },
+    theme: {
+      colors: {
+        primary: {
+          50: '#fef7e7',
+          100: '#fdecc4',
+          200: '#fbd686',
+          300: '#f9c048',
+          400: '#f7a91a',
+          500: '#EDB641',
+          600: '#c8851a',
+          700: '#a36a15',
+          800: '#7e4f10',
+          900: '#59340b',
+          950: '#341f06'
+        }
+      }
+    }
+  }
 })
