5c376f06d784ede9fe4640f53a085e9c97328e42
Julien <courtes.julien@gmail.com>
2025-09-13 20:27:35 +0200

    fix: remove description in blog ogImage

--- app/components/OgImage/OgImageBlog.vue
+++ app/components/OgImage/OgImageBlog.vue
@@ -17,18 +17,19 @@ withDefaults(
     <div class="absolute top-0 bottom-0 right-26 w-[2px] bg-gray-700" />
     <div class="absolute top-12 inset-x-0 h-[2px] bg-gray-700" />
     <div class="absolute bottom-12 inset-x-0 h-[2px] bg-gray-700" />
-    <div class="mx-26 mt-24 border-y-2 border-gray-700 h-14 flex flex-row items-center">
-      <div class="h-full uppercase flex items-center text-[#EAB308] text-[20px] font-semibold px-6 pt-1">
+    <div
+      class="mx-26 mt-24 border-y-2 border-gray-700 h-14 flex flex-row items-center"
+    >
+      <div
+        class="h-full uppercase flex items-center text-[#EAB308] text-[20px] font-semibold px-6 pt-1"
+      >
         Litestar Blog
       </div>
     </div>
-    <div class="mx-34 mt-6 h-[280px] flex flex-col justify-center">
+    <div class="mx-34 h-[290px] flex flex-col justify-center">
       <h1 class="text-5xl font-semibold mb-0 flex gap-1 text-white">
         <span>{{ blog.title }}</span>
       </h1>
-      <p class="text-3xl text-gray-400 line-clamp-2">
-        {{ blog.description }}
-      </p>
     </div>
     <div
       class="mx-26 mb-16 border-y-2 border-gray-700 h-14 flex flex-row items-center"
@@ -38,7 +39,6 @@ withDefaults(
       >
         <Logo class="h-[40px] w-[40px] mt-2" />
       </div>
-
 
       <div
         class="h-full uppercase flex items-center border-gray-700 text-[#EAB308] text-[20px] font-semibold px-6"
